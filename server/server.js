56941abcd917c85c4ae73f5e1932a59a3f74a9f1
Brice Christian <bricechristian@bricemac.attlocal.net>
2017-07-04 19:00:10 -0400

    Added deployment to heroku support

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 import bodyParser from 'body-parser'
 import express from 'express'
+import path from 'path'
 
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+
+const staticFiles = express.static(path.join(__dirname, '../../client/build'))
+app.use(staticFiles)
 
 const router = express.Router()
 router.get('/test', (req, res) => {
@@ -14,6 +18,9 @@ router.get('/test', (req, res) => {
 })
 
 app.use(router)
+
+app.use('/*', staticFiles)
+
 app.set('port', (process.env.PORT || 3001))
 app.listen(app.get('port'), () => {
   console.log(`Listening on ${app.get('port')}`)
